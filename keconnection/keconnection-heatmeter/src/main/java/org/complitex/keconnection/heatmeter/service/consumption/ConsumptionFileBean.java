fff0b033fee4934c96e624fa2d89f6e80fb82673
Anatoly Ivanov <java@inheaven.ru>
2015-05-08 19:35:47 +0600

    fix transaction update file status

--- keconnection/keconnection-heatmeter/src/main/java/org/complitex/keconnection/heatmeter/service/consumption/ConsumptionFileBean.java
+++ keconnection/keconnection-heatmeter/src/main/java/org/complitex/keconnection/heatmeter/service/consumption/ConsumptionFileBean.java
@@ -5,8 +5,6 @@ import org.complitex.common.service.AbstractBean;
 import org.complitex.keconnection.heatmeter.entity.consumption.ConsumptionFile;
 
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import java.util.List;
 
 /**
@@ -18,17 +16,17 @@ public class ConsumptionFileBean extends AbstractBean{
         return selectOne("selectConsumptionFile", id);
     }
 
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+//    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public List<ConsumptionFile> getConsumptionFiles(FilterWrapper<ConsumptionFile> filterWrapper){
         return selectList("selectConsumptionFiles", filterWrapper);
     }
 
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+//    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public Long getConsumptionFilesCount(FilterWrapper<ConsumptionFile> filterWrapper){
         return selectOne("selectConsumptionFilesCount", filterWrapper);
     }
 
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+//    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public void save(ConsumptionFile consumptionFile){
         if (consumptionFile.getId() == null) {
             insert("insertConsumptionFile", consumptionFile);
