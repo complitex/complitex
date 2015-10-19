5bd2bbf96a1b0080dc417a5d54cc955c420bfaf2
Anatoly Ivanov <java@inheaven.ru>
2015-10-19 18:29:31 +0600

    fix image resource

--- complitex/complitex-resources/src/main/java/org/complitex/resources/WebCommonResourceInitializer.java
+++ complitex/complitex-resources/src/main/java/org/complitex/resources/WebCommonResourceInitializer.java
@@ -3,7 +3,8 @@ package org.complitex.resources;
 import org.apache.wicket.Application;
 import org.apache.wicket.IInitializer;
 import org.apache.wicket.SharedResources;
-import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.request.resource.CssPackageResource;
+import org.apache.wicket.request.resource.JavaScriptPackageResource;
 import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.request.resource.SharedResourceReference;
 
@@ -35,14 +36,16 @@ public final class WebCommonResourceInitializer implements IInitializer {
     public static final String PLACEHOLDER_RESOURCE_NAME = "js/jquery.placeholder.js";
     public static final ResourceReference PLACEHOLDER_JS = newResourceReference(PLACEHOLDER_RESOURCE_NAME);
 
+
+
     @Override
     public void init(Application application) {
         SharedResources sharedResources = application.getSharedResources();
-        sharedResources.add(STYLE_RESOURCE_NAME, new PackageResourceReference(getClass(), STYLE_RELATIVE_PATH).getResource());
-        sharedResources.add(COMMON_RESOURCE_NAME, new PackageResourceReference(getClass(), COMMON_RELATIVE_PATH).getResource());
-        sharedResources.add(IE_SELECT_FIX_RESOURCE_NAME, new PackageResourceReference(getClass(), IE_SELECT_FIX_RELATIVE_PATH).getResource());
-        sharedResources.add(SCROLL_RESOURCE_NAME, new PackageResourceReference(getClass(), SCROLL_RELATIVE_PATH).getResource());
-        sharedResources.add(PLACEHOLDER_RESOURCE_NAME, new PackageResourceReference(getClass(), PLACEHOLDER_RELATIVE_PATH).getResource());
+        sharedResources.add(STYLE_RESOURCE_NAME, new CssPackageResource(getClass(), STYLE_RELATIVE_PATH, null, null, null));
+        sharedResources.add(COMMON_RESOURCE_NAME, new JavaScriptPackageResource(getClass(), COMMON_RELATIVE_PATH, null, null, null));
+        sharedResources.add(IE_SELECT_FIX_RESOURCE_NAME, new JavaScriptPackageResource(getClass(), IE_SELECT_FIX_RELATIVE_PATH, null, null, null));
+        sharedResources.add(SCROLL_RESOURCE_NAME, new JavaScriptPackageResource(getClass(), SCROLL_RELATIVE_PATH, null, null, null));
+        sharedResources.add(PLACEHOLDER_RESOURCE_NAME, new JavaScriptPackageResource(getClass(), PLACEHOLDER_RELATIVE_PATH, null, null, null));
     }
 
     private static ResourceReference newResourceReference(String resourceName) {
