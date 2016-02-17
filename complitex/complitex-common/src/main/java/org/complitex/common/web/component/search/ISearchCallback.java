1696a5ecd337332ec2e973744a9c835d915c2c49
Anatoly Ivanov <java@inheaven.ru>
2016-02-17 17:23:45 +0600

    fix street strategy search callback serializable

--- complitex/complitex-common/src/main/java/org/complitex/common/web/component/search/ISearchCallback.java
+++ complitex/complitex-common/src/main/java/org/complitex/common/web/component/search/ISearchCallback.java
@@ -6,10 +6,6 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import java.io.Serializable;
 import java.util.Map;
 
-/**
- *
- * @author Artem
- */
 public interface ISearchCallback extends Serializable{
 
     void found(Component component, Map<String, Long> ids, AjaxRequestTarget target);
