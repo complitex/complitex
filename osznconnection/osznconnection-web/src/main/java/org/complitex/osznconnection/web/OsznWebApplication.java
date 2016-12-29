d789be44d085d7c9072c94ae25f6c059615de113
Anatoly Ivanov <java@inheaven.ru>
2016-12-29 15:35:32 +0700

    test mount page

--- osznconnection/osznconnection-web/src/main/java/org/complitex/osznconnection/web/OsznWebApplication.java
+++ osznconnection/osznconnection-web/src/main/java/org/complitex/osznconnection/web/OsznWebApplication.java
@@ -1,11 +1,6 @@
 package org.complitex.osznconnection.web;
 
 import org.complitex.address.web.sync.AddressSyncPage;
-import org.complitex.osznconnection.file.web.pages.actualpayment.ActualPaymentList;
-import org.complitex.osznconnection.file.web.pages.facility.*;
-import org.complitex.osznconnection.file.web.pages.payment.PaymentList;
-import org.complitex.osznconnection.file.web.pages.privilege.*;
-import org.complitex.osznconnection.file.web.pages.subsidy.*;
 import org.complitex.template.web.ComplitexWebApplication;
 
 /**
@@ -19,46 +14,46 @@ public class OsznWebApplication extends ComplitexWebApplication{
         getDebugSettings().setAjaxDebugModeEnabled(false);
 
         //subsidy
-        mountPage("/subsidy/request", GroupList.class);
-        mountPage("/subsidy/request/payment/${request_file_id}", PaymentList.class);
-        mountPage("/subsidy/request/benefit/${request_file_id}", BenefitList.class);
-
-        mountPage("/subsidy/actual", ActualPaymentFileList.class);
-        mountPage("/subsidy/actual/${request_file_id}", ActualPaymentList.class);
-
-        mountPage("/subsidy/file", SubsidyFileList.class);
-        mountPage("/subsidy/file/${request_file_id}", SubsidyList.class);
-
-        mountPage("/subsidy/tarif", SubsidyTarifFileList.class);
-
-        //privilege
-        mountPage("/privilege/request", PrivilegeFileGroupList.class);
-        mountPage("/privilege/dwelling/${request_file_id}", DwellingCharacteristicsList.class);
-        mountPage("/privilege/service/${request_file_id}", FacilityServiceTypeList.class);
-
-        mountPage("/privilege/form-2", FacilityForm2FileList.class);
-        mountPage("/privilege/form-2/${request_file_id}", FacilityForm2List.class);
-
-        mountPage("/privilege/local", FacilityLocalFileList.class);
-        mountPage("/privilege/local/${request_file_id}", FacilityLocalList.class);
-
-        mountPage("/privilege/janitor", FacilityLocalJanitorFileList.class);
-        mountPage("/privilege/janitor/${request_file_id}", FacilityLocalJanitorList.class);
-
-        mountPage("/privilege/compensation", FacilityLocalCompensationFileList.class);
-        mountPage("/privilege/compensation/${request_file_id}", FacilityLocalCompensationList.class);
-
-        mountPage("/privilege/street-type", FacilityStreetTypeFileList.class);
-        mountPage("/privilege/street-type/${request_file_id}", FacilityStreetTypeList.class);
-
-        mountPage("/privilege/street", FacilityStreetFileList.class);
-        mountPage("/privilege/street/${request_file_id}", FacilityStreetList.class);
-
-        mountPage("/privilege/tarif", FacilityTarifFileList.class);
-        mountPage("/privilege/tarif/${request_file_id}", FacilityTarifList.class);
-
-        mountPage("/privilege/prolongation", PrivilegeProlongationFileList.class); //todo add types
-        mountPage("/privilege/prolongation/${request_file_id}", PrivilegeProlongationList.class);
+//        mountPage("/subsidy/request", GroupList.class);
+//        mountPage("/subsidy/request/payment/${request_file_id}", PaymentList.class);
+//        mountPage("/subsidy/request/benefit/${request_file_id}", BenefitList.class);
+//
+//        mountPage("/subsidy/actual", ActualPaymentFileList.class);
+//        mountPage("/subsidy/actual/${request_file_id}", ActualPaymentList.class);
+//
+//        mountPage("/subsidy/file", SubsidyFileList.class);
+//        mountPage("/subsidy/file/${request_file_id}", SubsidyList.class);
+//
+//        mountPage("/subsidy/tarif", SubsidyTarifFileList.class);
+//
+//        //privilege
+//        mountPage("/privilege/request", PrivilegeFileGroupList.class);
+//        mountPage("/privilege/dwelling/${request_file_id}", DwellingCharacteristicsList.class);
+//        mountPage("/privilege/service/${request_file_id}", FacilityServiceTypeList.class);
+//
+//        mountPage("/privilege/form-2", FacilityForm2FileList.class);
+//        mountPage("/privilege/form-2/${request_file_id}", FacilityForm2List.class);
+//
+//        mountPage("/privilege/local", FacilityLocalFileList.class);
+//        mountPage("/privilege/local/${request_file_id}", FacilityLocalList.class);
+//
+//        mountPage("/privilege/janitor", FacilityLocalJanitorFileList.class);
+//        mountPage("/privilege/janitor/${request_file_id}", FacilityLocalJanitorList.class);
+//
+//        mountPage("/privilege/compensation", FacilityLocalCompensationFileList.class);
+//        mountPage("/privilege/compensation/${request_file_id}", FacilityLocalCompensationList.class);
+//
+//        mountPage("/privilege/street-type", FacilityStreetTypeFileList.class);
+//        mountPage("/privilege/street-type/${request_file_id}", FacilityStreetTypeList.class);
+//
+//        mountPage("/privilege/street", FacilityStreetFileList.class);
+//        mountPage("/privilege/street/${request_file_id}", FacilityStreetList.class);
+//
+//        mountPage("/privilege/tarif", FacilityTarifFileList.class);
+//        mountPage("/privilege/tarif/${request_file_id}", FacilityTarifList.class);
+//
+//        mountPage("/privilege/prolongation", PrivilegeProlongationFileList.class); //todo add types
+//        mountPage("/privilege/prolongation/${request_file_id}", PrivilegeProlongationList.class);
 
 
         //correction
