39eabf70abc22b56484bcb4bfb0db5055d101a22
Anatoly Ivanov <java@inheaven.ru>
2017-04-27 20:38:19 +0700

    init pspoffice api

---
+++ pspoffice/pspoffice-backend/src/main/java/ru/complitex/pspoffice/api/PspApiApplication.java
@@ -0,0 +1,21 @@
+package ru.complitex.pspoffice.api;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import ru.complitex.pspoffice.api.resource.AddressResource;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * @author Anatoly A. Ivanov
+ *         27.04.2017 19:54
+ */
+@ApplicationPath("api")
+public class PspApiApplication extends ResourceConfig {
+    public PspApiApplication() {
+        //swagger
+        register(io.swagger.jaxrs.listing.ApiListingResource.class);
+        register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+
+        register(AddressResource.class);
+    }
+}
---
+++ pspoffice/pspoffice-backend/src/main/java/ru/complitex/pspoffice/api/resource/AddressResource.java
@@ -0,0 +1,36 @@
+package ru.complitex.pspoffice.api.resource;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+/**
+ * @author Anatoly A. Ivanov
+ *         27.04.2017 19:07
+ */
+@Path("/address")
+@Consumes(APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
+@Api(description = "Address Database API")
+public class AddressResource {
+
+    @GET
+    @Path("/country/${id}")
+    @ApiOperation("Get country by id")
+    public Response getCountry(@PathParam("id") Long id){
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("/country")
+    @ApiOperation("Get county list by query string")
+    public Response getCountries(@QueryParam("query") String query, @QueryParam("limit") Integer limit){
+        return Response.ok().build();
+    }
+
+
+}
