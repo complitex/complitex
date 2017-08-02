464896a3256ee6110fa9fca48a2242e5201b0e08
Anatoly Ivanov <java@inheaven.ru>
2017-08-02 18:21:57 +0700

    dev name object

---
+++ pspoffice/pspoffice-api/src/main/java/ru/complitex/pspoffice/api/model/NameList.java
@@ -0,0 +1,13 @@
+package ru.complitex.pspoffice.api.model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Anatoly A. Ivanov
+ * 02.08.2017 17:24
+ */
+public class NameList extends ArrayList<Name>{
+    public void getName(Long localeId){
+
+    }
+}
