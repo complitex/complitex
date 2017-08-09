d3d7f0668cca7b9cef9b90dfc99411f65f6f0430
Anatoly Ivanov <java@inheaven.ru>
2017-08-09 17:06:12 +0700

    add filter id column class

--- pspoffice/pspoffice-frontend/src/main/java/org/complitex/pspoffice/frontend/web/address/AddressListPage.java
+++ pspoffice/pspoffice-frontend/src/main/java/org/complitex/pspoffice/frontend/web/address/AddressListPage.java
@@ -1,7 +1,10 @@
 package org.complitex.pspoffice.frontend.web.address;
 
 import com.google.common.collect.Sets;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.TextFilteredPropertyColumn;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.complitex.pspoffice.frontend.web.BasePage;
@@ -29,8 +32,22 @@ public class AddressListPage extends BasePage{
             throw new RuntimeException("wrong address entity");
         }
 
-        add(new TablePanel<>("addresses", AddressObject.class, Arrays.asList("id", "name.ru"),
-                new AddressDataProvider(entity)));
+        add(new TablePanel<AddressObject>("addresses", AddressObject.class, Arrays.asList("id", "name.ru"),
+                new AddressDataProvider(entity)){
+            @Override
+            protected IColumn<AddressObject, String> getColumn(String field) {
+                if ("id".equals(field)){
+                    return new TextFilteredPropertyColumn<AddressObject, String, String>(Model.of("#"), "id", "id"){
+                        @Override
+                        public String getCssClass() {
+                            return "filter-td-id";
+                        }
+                    };
+                }
+
+                return super.getColumn(field);
+            }
+        });
     }
 
     @Override
