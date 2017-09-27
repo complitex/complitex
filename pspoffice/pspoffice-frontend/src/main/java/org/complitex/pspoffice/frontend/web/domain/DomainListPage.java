d4363240e2241b19579f1dba6466c461f95f18cb
Anatoly Ivanov <java@inheaven.ru>
2017-09-27 18:15:31 +0700

    dev domain list page

---
+++ pspoffice/pspoffice-frontend/src/main/java/org/complitex/pspoffice/frontend/web/domain/DomainListPage.java
@@ -0,0 +1,15 @@
+package org.complitex.pspoffice.frontend.web.domain;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.complitex.pspoffice.frontend.web.BasePage;
+
+/**
+ * @author Anatoly A. Ivanov
+ * 27.09.2017 15:53
+ */
+public class DomainListPage extends BasePage{
+    public DomainListPage(PageParameters pageParameters) {
+        //todo list page by entity id
+
+    }
+}
