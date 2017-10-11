60eb6d4f268dba11dc5c951a7c278d28d1d61b96
Anatoly Ivanov <java@inheaven.ru>
2017-10-11 14:55:29 +0700

    add domain list resource

--- pspoffice/pspoffice-backend/src/main/java/ru/complitex/pspoffice/backend/resource/DomainResource.java
+++ pspoffice/pspoffice-backend/src/main/java/ru/complitex/pspoffice/backend/resource/DomainResource.java
@@ -3,7 +3,7 @@ package ru.complitex.pspoffice.backend.resource;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.complitex.common.entity.DomainObject;
-import org.complitex.common.strategy.IStrategy;
+import org.complitex.common.entity.DomainObjectFilter;
 import org.complitex.common.strategy.StrategyFactory;
 import ru.complitex.pspoffice.api.model.DomainModel;
 import ru.complitex.pspoffice.backend.adapter.DomainAdapter;
@@ -11,6 +11,7 @@ import ru.complitex.pspoffice.backend.adapter.DomainAdapter;
 import javax.ejb.EJB;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.stream.Collectors;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -35,15 +36,34 @@ public class DomainResource {
     @GET
     @Path("{entity}/{id}")
     @ApiOperation(value = "Get domain model by entity name and object id", response = DomainModel.class)
-    public Response getDomainModel(@PathParam("entity") String entity, @PathParam("id") Long id){
-        IStrategy strategy = strategyFactory.getStrategy(entity);
-
-        DomainObject domainObject = strategy.getDomainObject(id, true);
+    public Response getDomain(@PathParam("entity") String entity, @PathParam("id") Long id){
+        DomainObject domainObject = strategyFactory.getStrategy(entity).getDomainObject(id, true);
 
         if (domainObject == null){
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
         return Response.ok(DomainAdapter.adapt(domainObject)).build();
+    }
+
+    @GET
+    @Path("{entity}")
+    @ApiOperation(value = "Get domain model list by entity name and object id", response = DomainModel.class, responseContainer = "List")
+    public Response getDomains(@PathParam("entity") String entity,
+                               @QueryParam("offset") @DefaultValue("0") Long offset,
+                               @QueryParam("count") @DefaultValue("10") Long count){
+        DomainObjectFilter filter = new DomainObjectFilter();
+        filter.setFirst(offset);
+        filter.setCount(count);
+
+        return  Response.ok(strategyFactory.getStrategy(entity).getList(filter).stream().map(DomainAdapter::adapt)
+                .collect(Collectors.toList())).build();
+    }
+
+    @GET
+    @Path("{entity}/size")
+    @ApiOperation(value = "Get domain model count", response = DomainModel.class)
+    public Response getDomainsCount(@PathParam("entity") String entity){
+        return Response.ok(strategyFactory.getStrategy(entity).getCount(new DomainObjectFilter())).build();
     }
 }
