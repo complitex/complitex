acef46ff209f9da81f01942a5350edbf76d43d6b
Anatoly Ivanov <java@inheaven.ru>
2017-10-30 18:31:33 +0700

    dev person model reference

--- pspoffice/pspoffice-backend/src/main/java/ru/complitex/pspoffice/backend/resource/ApartmentCardResource.java
+++ pspoffice/pspoffice-backend/src/main/java/ru/complitex/pspoffice/backend/resource/ApartmentCardResource.java
@@ -3,7 +3,12 @@ package ru.complitex.pspoffice.backend.resource;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.complitex.common.entity.DomainObject;
+import org.complitex.common.entity.EntityAttribute;
+import org.complitex.common.entity.ValueType;
+import org.complitex.common.strategy.EntityBean;
+import org.complitex.common.strategy.StrategyFactory;
 import org.complitex.pspoffice.person.strategy.ApartmentCardStrategy;
+import org.complitex.pspoffice.person.strategy.service.PersonNameBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.complitex.pspoffice.api.model.DomainModel;
@@ -29,6 +34,15 @@ public class ApartmentCardResource {
     @EJB
     private ApartmentCardStrategy apartmentCardStrategy;
 
+    @EJB
+    private EntityBean entityBean;
+
+    @EJB
+    private StrategyFactory strategyFactory;
+
+    @EJB
+    private PersonNameBean personNameBean;
+
     @GET
     @Path("ping")
     public Response ping(){
@@ -39,12 +53,31 @@ public class ApartmentCardResource {
     @Path("{id}")
     @ApiOperation(value = "Get apartment card by id", response = DomainModel.class)
     public Response getApartmentCard(@PathParam("id") Long id){
-        DomainObject o = apartmentCardStrategy.getDomainObject(id);
+        DomainObject domainObject = apartmentCardStrategy.getDomainObject(id);
 
-        if (o == null){
+        if (domainObject == null){
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return Response.ok(DomainAdapter.adapt(o)).build();
+        DomainModel domainModel = DomainAdapter.adapt(domainObject);
+
+        domainModel.getAttributes().forEach(a -> {
+            EntityAttribute ea = entityBean.getAttributeType(a.getEntityAttributeId());
+
+            if (ea.getValueType().equals(ValueType.ENTITY)){
+                DomainObject o = strategyFactory.getStrategy(entityBean.getEntity(ea.getReferenceId()).getEntity())
+                        .getDomainObject(a.getValueId(), true);
+
+                if (o != null) {
+                    a.setReference(DomainAdapter.adapt(o));
+                }
+            }
+
+            //todo person model
+
+        });
+
+        return Response.ok(domainModel).build();
     }
 }
+
