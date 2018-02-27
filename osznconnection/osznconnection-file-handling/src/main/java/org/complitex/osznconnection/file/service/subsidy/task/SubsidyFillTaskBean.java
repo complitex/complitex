0c95d3175bed3c2e9f190e4be917ab8ba0e03987
Anatoly Ivanov <java@inheaven.ru>
2018-02-27 21:48:46 +0700

    fix subsidy fill empty dat1 dat2

--- osznconnection/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/subsidy/task/SubsidyFillTaskBean.java
+++ osznconnection/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/subsidy/task/SubsidyFillTaskBean.java
@@ -95,6 +95,15 @@ public class SubsidyFillTaskBean extends AbstractRequestTaskBean<RequestFile> {
     }
 
     public void fill(RequestFile requestFile, Subsidy subsidy) {
+        if (subsidy.getDateField(SubsidyDBF.DAT1) == null || subsidy.getDateField(SubsidyDBF.DAT2) == null){
+            subsidy.setStatus(RequestStatus.PROCESSED_WITH_ERROR);
+            subsidyBean.update(subsidy);
+
+            log.error("subsidy fill error: empty dat1 or dat2 {}", subsidy);
+
+            return;
+        }
+
         LocalDate dat1 = new Date(subsidy.getDateField(SubsidyDBF.DAT1).getTime()).toInstant()
                 .atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate dat2 = new Date(subsidy.getDateField(SubsidyDBF.DAT2).getTime()).toInstant()
