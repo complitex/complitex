873a79fad2a5836a86aa90c65359ebe287daa30d
Anatoly Ivanov <java@inheaven.ru>
2018-02-28 15:21:17 +0700

    fix create priv header result codes

--- osznconnection/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/privilege/task/PrivilegeProlongationSaveTaskBean.java
+++ osznconnection/osznconnection-file-handling/src/main/java/org/complitex/osznconnection/file/service/privilege/task/PrivilegeProlongationSaveTaskBean.java
@@ -84,19 +84,26 @@ public class PrivilegeProlongationSaveTaskBean extends AbstractRequestTaskBean<R
             //noinspection Duplicates
             switch (collectionId.intValue()){
                 case -5: //Не найден р-он
-                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_district_not_found"), district);
+                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_district_not_found"),
+                            requestFile.getFullName(), district);
                 case -15: //Дублируется имя файла для заданного месяца
                     throw new SaveException(ResourceUtil.getString(RESOURCE, "error_filename_duplicate"),
                             requestFile.getFullName(), requestFile.getBeginDate());
                 case -16: //Неправильное кол-во записей в файле
                     throw new SaveException(ResourceUtil.getString(RESOURCE, "error_record_count"),
-                            requestFile.getLoadedRecordCount());
+                            requestFile.getFullName(), requestFile.getLoadedRecordCount());
+                case -17: //Не указана зависимость от дохода
+                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_no_dependency_on_income"),
+                            requestFile.getFullName());
                 case -18: //Не указан месяц файла
-                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_null_month"));
+                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_null_month"),
+                            requestFile.getFullName());
                 case -19: //Не указано имя файла
-                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_null_filename"));
+                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_null_filename"),
+                            requestFile.getFullName());
                 case -20: //Не определена организация
-                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_organization_undefined"), zheuCode);
+                    throw new SaveException(ResourceUtil.getString(RESOURCE, "error_organization_undefined"),
+                            requestFile.getFullName(), zheuCode);
                 default:
                     throw new SaveException("Код ошибки {}", collectionId);
             }
