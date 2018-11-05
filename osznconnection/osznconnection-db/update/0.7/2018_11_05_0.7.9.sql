e39989328cee263171af4b057502eaa26579347c
Anatoly Ivanov <java@inheaven.ru>
2018-11-05 22:37:42 +0700

    fix ownership correction start date

--- osznconnection/osznconnection-db/update/0.7/2018_11_05_0.7.9.sql
+++ osznconnection/osznconnection-db/update/0.7/2018_11_05_0.7.9.sql
@@ -1,7 +1,7 @@
-ALTER TABLE ownership_correction CHANGE COLUMN begin_date start_date DATE NOT NULL DEFAULT '1970-01-01' COMMENT 'Дата начала актуальности соответствия';
+ALTER TABLE ownership_correction CHANGE COLUMN begin_date start_date DATE NULL DEFAULT '1970-01-01' COMMENT 'Дата начала актуальности соответствия';
 ALTER TABLE ownership_correction ADD KEY `key_start_date` (`start_date`);
 
-ALTER TABLE privilege_correction CHANGE COLUMN begin_date start_date DATE NOT NULL DEFAULT '1970-01-01' COMMENT 'Дата начала актуальности соответствия';
+ALTER TABLE privilege_correction CHANGE COLUMN begin_date start_date DATE NULL DEFAULT '1970-01-01' COMMENT 'Дата начала актуальности соответствия';
 ALTER TABLE privilege_correction DROP KEY key_begin_date;
 ALTER TABLE privilege_correction ADD KEY `key_start_date` (`start_date`);
 
