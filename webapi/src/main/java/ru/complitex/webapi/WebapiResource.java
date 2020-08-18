26cec5cf48a1ff24fdb6d9cd1339823a25db538b
Anatoly A. Ivanov <java@inheaven.ru>
2020-08-18 19:28:36 +0700

    add getZerro

--- webapi/src/main/java/ru/complitex/webapi/WebapiResource.java
+++ webapi/src/main/java/ru/complitex/webapi/WebapiResource.java
@@ -6,12 +6,14 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.math.BigDecimal;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -37,7 +39,7 @@ public class WebapiResource {
         }
 
         try (Connection connection = this.dataSource.getConnection()) {
-            CallableStatement ps = connection.prepareCall(
+            @SuppressWarnings("SqlResolve") CallableStatement ps = connection.prepareCall(
                     "{? = call COMP.z$runtime_sz_utl.getAccDebtToday(?, ?, ?)}"
             );
 
@@ -55,6 +57,45 @@ public class WebapiResource {
                     .add("acc",  acc != null ? acc : "")
                     .add("debt", debt != null ? debt : "")
                     .build();
+        }
+    }
+
+    @Path("/getZerro")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getZerro(@QueryParam("accId") String accId) throws SQLException {
+        if (accId == null || accId.isEmpty()){
+            return null;
+        }
+
+        try (Connection connection = this.dataSource.getConnection()) {
+            @SuppressWarnings("SqlResolve") CallableStatement ps = connection.prepareCall(
+                    "{? = call COMP.z$runtime_sz_utl.getAccDebtToday(?, ?, ?)}"
+            );
+
+            ps.registerOutParameter(1, Types.INTEGER);
+            ps.setString(2, accId);
+            ps.registerOutParameter(3, Types.VARCHAR);
+            ps.registerOutParameter(4, Types.VARCHAR);
+
+            ps.execute();
+
+            String acc = ps.getString(3);
+            String debt = ps.getString(4);
+
+            JsonObjectBuilder json =  Json.createObjectBuilder();
+
+            json.add("accId", accId);
+
+            if (debt != null && new BigDecimal(debt).compareTo(BigDecimal.ZERO) > 0){
+                json.add("result", "0");
+
+                json.add("conditions", Json.createArrayBuilder().add(Json.createObjectBuilder().add("saldo", debt)));
+            } else {
+                json.add("result", "1");
+            }
+
+            return json.build();
         }
     }
 }
